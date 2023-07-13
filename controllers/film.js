a7d55d6fc8acf6aa1b035396c95b28ea911b3a84
gnahcty <gnahcty@gmail.com>
2023-07-13 16:30:43 +0800

    mdb

--- controllers/film.js
+++ controllers/film.js
@@ -1,24 +1,39 @@
-import Moviedb from 'moviedb'
+import 'dotenv/config'
+import { MovieDb } from 'moviedb-promise'
 import { StatusCodes } from 'http-status-codes'
+const mdb = new MovieDb(process.env.TMDB_API_KEY)
 
-const mdb = new Moviedb(process.env.TMDB_API_KEY)
-
-export const searchMovie = async (req, res) => {
-  console.log('searching movie')
+export const getFilms = async (req, res) => {
   try {
-    // const { query } = req.query
-    const results = await mdb.searchMovie({ query: req.params.name })
-    console.log(results)
-    if (!results) {
-      throw new Error('NOT FOUND')
+    const params = {
+      page: 1,
+      region: req.params.region,
+      with_genres: '',
+      year: req.params.year
+      // vote_average_gte: 0
     }
+    const results = await mdb.discoverMovie(params)
     res.status(StatusCodes.OK).json({
       success: true,
       message: '',
       results
     })
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({ message: err.message })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const searchMovie = async (req, res) => {
+  try {
+    const results = await mdb.searchMovie(req.params.title)
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      results
+    })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ message: error.message })
   }
 }
--- routes/filmsRoute.js
+++ routes/filmsRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { searchMovie } from '../controllers/film.js'
+import { searchMovie, getFilms } from '../controllers/film.js'
 const router = express.Router()
-router.get('/:name', searchMovie)
+router.get('/search/:title', searchMovie)
+router.get('/films', getFilms)
 
 export default router
